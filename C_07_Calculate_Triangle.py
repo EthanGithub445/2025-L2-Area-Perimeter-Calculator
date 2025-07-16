9b2aae7b2e2eca61a59bf8bdc9db668953dc37c5
EthanGithub445 <159392290+EthanGithub445@users.noreply.github.com>
2025-07-16 14:48:24 +1200

    uploaded program components
    updated calculate triangle function

--- C_07_Calculate_Triangle.py
+++ C_07_Calculate_Triangle.py
@@ -1,8 +1,16 @@
 # Function to calculate for triangle
-def calculate_triangle(a, b, c, height):
+def calculate_triangle():
+    a = float(input("Side a: "))
+    b = float(input("Side b: "))
+    c = float(input("Side c: "))
+    height = float(input("Height: "))
     # Area using half base x height
     area = 0.5 * (b * height)
 
     # Perimeter by adding all sides (side b for base)
     perimeter = a + b + c
+    print(f"Area: {area}")
+    print(f"Perimeter: {perimeter}")
+
     return ["Triangle", f"Sides={a}, {b}, {c}, Height={height}", (area, 2), (perimeter, 2)]
+calculate_triangle()
